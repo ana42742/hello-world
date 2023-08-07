2bb2ae9654695a8116ca26fd1bfe7a1abdf58faf
ana42742 <rebellajoy@gmail.com>
2023-08-07 10:49:56 +0530

    Login request handler

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ use flowsnet_platform_sdk::logger;
 use std::collections::HashMap;
 use serde_json::Value;
 
+const VALID_USERNAME: &str = "abc";
+const VALID_PASSWORD: &str = "pass";
+
 #[no_mangle]
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() -> anyhow::Result<()> {
@@ -16,13 +19,36 @@ async fn handler(headers: Vec<(String, String)>, qry: HashMap<String, Value>, _b
     logger::init();
     log::info!("Headers -- {:?}", headers);
 
-    let msg = qry.get("msg").unwrap();
-    // let msg = String::from_utf8(body).unwrap_or("".to_string());
-    let resp = format!("Welcome to flows.network.\nYou just said: '{}'.\nLearn more at: https://github.com/flows-network/hello-world\n", msg);
+    // let msg = qry.get("msg").unwrap();
+    let username = qry.get("username").and_then(|v| v.as_str());
+    let password = qry.get("password").and_then(|v| v.as_str());
 
+    if let (Some(username), Some(password)) = (username, password) {
+        // Validate the provided credentials against predefined credentials
+        if username == VALID_USERNAME && password == VALID_PASSWORD {
+            let resp = format!("Login successful for user: {}", username);
+            send_response(
+                200,
+                vec![(String::from("content-type"), String::from("text/html"))],
+                resp.as_bytes().to_vec(),
+            );
+            return;
+        }
+    }
+
+    let resp = "Login failed: Invalid username or password";
     send_response(
-        200,
+        401, // Unauthorized status code
         vec![(String::from("content-type"), String::from("text/html"))],
         resp.as_bytes().to_vec(),
     );
+
+    // let msg = String::from_utf8(body).unwrap_or("".to_string());
+    // let resp = format!("Welcome to flows.network.\nYou just said: '{}'.\nLearn more at: https://github.com/flows-network/hello-world\n", msg);
+
+    // send_response(
+    //     200,
+    //     vec![(String::from("content-type"), String::from("text/html"))],
+    //     resp.as_bytes().to_vec(),
+    // );
 }
